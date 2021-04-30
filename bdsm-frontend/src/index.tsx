da1da35ead5b8b9679d9639862384a35bfbae95f
ikmokhirio <ikmokhirio@mail.ru>
2021-05-01 02:13:29 +0300

    Removed strict mode to disable errors in console while testing

--- bdsm-frontend/src/index.tsx
+++ bdsm-frontend/src/index.tsx
@@ -7,12 +7,13 @@ import {Provider} from "react-redux";
 import {store} from "./store";
 
 ReactDom.render((
- <React.StrictMode>
-     <Router>
-         <Provider store={store}>
-             <App/>
-         </Provider>
-     </Router>
- </React.StrictMode>),
- document.getElementById('root')
+        // <React.StrictMode>
+        <Router>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </Router>
+        // </React.StrictMode>
+    ),
+    document.getElementById('root')
 )
