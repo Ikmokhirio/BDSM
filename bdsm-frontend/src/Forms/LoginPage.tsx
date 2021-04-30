e795fce98cbb6f6e3a58e13e027920116d5cf0cb
ikmokhirio <ikmokhirio@mail.ru>
2021-05-01 02:13:06 +0300

    Added authorization logic

--- bdsm-frontend/src/Forms/LoginPage.tsx
+++ bdsm-frontend/src/Forms/LoginPage.tsx
@@ -1,63 +1,98 @@
-import React, {Component} from "react";
+import React, {Component, useEffect, useState} from "react";
 import {Form, Input, Button, Checkbox, Row, Col} from 'antd';
 import {UserOutlined, LockOutlined} from "@ant-design/icons";
 import {Link} from "react-router-dom";
+import {loginUserInformation} from "../store/reducers/UserReducer";
+import {useActions} from "../hooks/useActions";
 
-const LoginPage = () => (
-    <div className={"loginPage"}>
+const LoginPage = () => {
+    const [state, setState] = useState<loginUserInformation>({
+        username: "",
+        password: ""
+    })
 
-        <Row>
-            <Col span={12}>
-                <img className={"formImage"} src={'login.png'} alt={"Some cool image"}/>
-            </Col>
+    const {loginUser} = useActions();
 
-            <Col span={6}>
+    const handleClick = (event: any) => {
+        event.preventDefault();
+        loginUser(state);
+    }
+    return(
+        <div className={"loginPage"}>
 
-                <Form
-                    name="normal_login"
-                    className="login-form"
-                >
-                    <h1>Добро пожаловать!</h1>
+            <Row>
+                <Col span={12}>
+                    <img className={"formImage"} src={'login.png'} alt={"Some cool image"}/>
+                </Col>
 
-                    <Form.Item
-                        name="username"
-                        rules={[{required: true, message: 'Введите имя пользователя!'}]}
+                <Col span={6}>
+
+                    <Form
+                        name="normal_login"
+                        className="login-form"
                     >
-                        <Input prefix={<UserOutlined className="site-form-item-icon"/>}
-                               placeholder="Имя пользователя"/>
-                    </Form.Item>
+                        <h1>Добро пожаловать!</h1>
 
-                    <Form.Item
-                        name="password"
-                        rules={[{required: true, message: 'Введите пароль!'}]}
-                    >
-                        <Input.Password
-                            prefix={<LockOutlined className="site-form-item-icon"/>}
-                            type="password"
-                            placeholder="Пароль"
+                        <Form.Item
+                            name="username"
+                            rules={[{required: true, message: 'Введите имя пользователя!'}]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon"/>}
+                                   placeholder="Имя пользователя"
+                                   onChange={(event: any) => {
+                                       let value = ""
+                                       if (event.target.value != null) {
+                                           value = event.target.value;
+                                       }
+                                       setState({
+                                           username: value,
+                                           password: state.password
+                                       })
+                                   }}/>
+                        </Form.Item>
 
-                        />
-                    </Form.Item>
-                    <Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[{required: true, message: 'Введите пароль!'}]}
+                        >
+                            <Input.Password
+                                prefix={<LockOutlined className="site-form-item-icon"/>}
+                                type="password"
+                                placeholder="Пароль"
+                                onChange={(event: any) => {
+                                    let value = ""
+                                    if (event.target.value != null) {
+                                        value = event.target.value;
+                                    }
+                                    setState({
+                                        username: state.username,
+                                        password: value
+                                    })
+                                }}
 
-                        <a className="login-form-forgot" href="/">
-                            Забыли пароль?
-                        </a>
-                    </Form.Item>
+                            />
+                        </Form.Item>
+                        <Form.Item>
 
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button">
-                            Войти
-                        </Button>
-                        или <Link to={"/register"}>зарегистрироваться</Link>
-                    </Form.Item>
-                </Form>
-            </Col>
-        </Row>
+                            <a className="login-form-forgot" href="/">
+                                Забыли пароль?
+                            </a>
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button" onClick={handleClick}>
+                                Войти
+                            </Button>
+                            или <Link to={"/register"}>зарегистрироваться</Link>
+                        </Form.Item>
+                    </Form>
+                </Col>
+            </Row>
 
 
-    </div>
-)
+        </div>
+    )
+}
 
 
 export default LoginPage;
