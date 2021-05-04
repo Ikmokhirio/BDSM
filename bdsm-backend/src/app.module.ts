c366990e2c737d3df1710ae2eab459bcdbb721be
ikmokhirio <ikmokhirio@mail.ru>
2021-05-04 16:24:33 +0300

    Fixed incorrect structure (caused failure on compilation)

--- bdsm-backend/src/app.module.ts
+++ bdsm-backend/src/app.module.ts
@@ -4,12 +4,9 @@ import {AppService} from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UsersModule} from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import {AuthController} from "./auth/auth.controller";
 import { MailsModule } from './mails/mails.module';
 import { TargetsModule } from './targets/targets.module';
 import { GroupsModule } from './groups/groups.module';
-import { TasksService } from './tasks/tasks.service';
-import { TasksController } from './tasks/tasks.controller';
 import { TasksModule } from './tasks/tasks.module';
 import { MailerModule } from './mailer/mailer.module';
 
@@ -24,8 +21,8 @@ import { MailerModule } from './mailer/mailer.module';
         TasksModule,
         MailerModule
     ],
-    controllers: [AppController, TasksController],
-    providers: [AppService, TasksService],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {
 }
--- bdsm-backend/src/tasks/tasks.module.ts
+++ bdsm-backend/src/tasks/tasks.module.ts
@@ -1,4 +1,17 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Groups} from "../groups/entities/groups.entity";
+import {Tasks} from "./entities/tasks.entity";
+import {Mails} from "../mails/entities/mails.entity";
+import {Users} from "../users/entities/users.entity";
+import {TasksController} from "./tasks.controller";
+import {TasksService} from "./tasks.service";
 
-@Module({})
-export class TasksModule {}
+@Module({
+    imports: [TypeOrmModule.forFeature([Tasks, Mails, Groups, Users])],
+    controllers: [TasksController],
+    providers: [TasksService],
+    exports: [TasksService]
+})
+export class TasksModule {
+}
